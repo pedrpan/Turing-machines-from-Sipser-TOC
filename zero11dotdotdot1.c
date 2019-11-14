c4300b93ab7d15d4c91af1a3e65e668778d8a462
Pedro Juan Soto <34007787+pedrpan@users.noreply.github.com>
2019-11-14 18:30:44 -0500

    Update zero11dotdotdot1.c

--- zero11dotdotdot1.c
+++ zero11dotdotdot1.c
@@ -4,30 +4,32 @@
 
 int zero11dotdotdot1(char s[], int i);
 
-main()
-{
-  int stringsize, keylen, i=1;
+int main(){
+  int stringsize, i=1;
   char *string = malloc(1);
 
   while((string[i-1] = getchar()) != EOF)
     string = realloc(string, ++i);
   string[i-1]='\0';
   stringsize = i-2;
+
   if (zero11dotdotdot1(string, i)==1)
     printf("machine accepts\n");
   else
     printf("machine rejects\n");
+
+  return 0;
 }
 
 int zero11dotdotdot1(char s[], int i){
   int j = 0;
   int q = 0;
   printf("M is in state %d\n and our string is equal to \n", q);
-  printf(s);
+  printf("%s", s);
   printf("\n");
   while(1){
     printf("M is in state %d\n and our string is equal to \n", q);
-    printf(s);
+    printf("%s", s);
     printf("\n");
     if (q == 0){
       if (s[j] == '0')
